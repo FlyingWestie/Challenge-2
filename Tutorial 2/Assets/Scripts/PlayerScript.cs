56e5112cb43d3bb00580cfaa2fc371c7489a26db
Steven Pineda <spineda0199@gmail.com>
2019-10-27 14:25:50 -0400

    Challenge 2

--- Tutorial 2/Assets/Scripts/PlayerScript.cs
+++ Tutorial 2/Assets/Scripts/PlayerScript.cs
@@ -9,15 +9,30 @@ public class PlayerScript : MonoBehaviour
 
     public float speed;
 
-    public Text score;
+    public AudioClip MusicClip;
 
-    private int scoreValue = 0;
+    public AudioSource MusicSource;
+
+    public Text countText;
+
+    public Text winText;
+
+    public Text livesText;
+
+    private int count;
+
+    private int lives;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        SetCountText();
+        SetLivesText();
+        MusicSource.clip = MusicClip;
     }
 
     // Update is called once per frame
@@ -34,8 +49,15 @@ public class PlayerScript : MonoBehaviour
     {
         if (collision.collider.tag == "Coin")
         {
-            scoreValue += 1;
-            score.text = scoreValue.ToString();
+            count = count + 1;
+            SetCountText();
+            Destroy(collision.collider.gameObject);
+        }
+
+        else if (collision.collider.tag == "Enemy")
+        {
+            lives = lives - 1;
+            SetLivesText();
             Destroy(collision.collider.gameObject);
         }
 
@@ -54,6 +76,33 @@ public class PlayerScript : MonoBehaviour
             {
                 Application.Quit();
             }
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count == 4)
+        {
+            transform.position = new Vector2(100.0f, 0.0f);
+            lives = 3;
+            SetLivesText();
+        }
+
+        if (count >= 8)
+        {
+            MusicSource.Play();
+            winText.text = "You win! Game created by Steven Pineda!";
+        }
+    }
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives <= 0)
+        {
+            Destroy(this);
+            winText.text = "You Lose, Try Again!";
         }
     }
 
